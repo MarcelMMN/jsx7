b02cf22a2ce9a051493445f0712a61a073a3b65a
MarcelMMN <marcelmatejakniemczuk@gmail.com>
2026-07-14 11:36:36 +0200

    form

--- App.jsx
+++ App.jsx
@@ -1,13 +1,17 @@
-import { useState } from "react";
+import { useState } from "react"; 
 
 function App() {
-  const [name, setName] = useState("");
+
+  function handleSubmit (event) {
+    event.preventDefault(); 
+    alert("Form Submitted!");
+  };
 
   return (
-    <div>
-      <input placeholder="Enter Name" onChange={ (e) => setName(e.target.value)}/>
-      <h2>Hello {name}</h2>
-    </div>
+    <form onSubmit={handleSubmit}>
+      <input type="text" placeholder="Name" />
+      <button type="submit">Submit</button>
+    </form>
   );
 }
 
